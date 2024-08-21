709725ef4d2a75cbb51aa6759ab0ef4662b574e5
Remy <rumena.mp@gmail.com>
2024-08-21 10:18:37 +0200

    opg.6&7

---
+++ L01Opg6/MyList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L01Opg6
+{
+    public class MyList
+    {
+        // private list of integers
+        private List<int> numbers;
+
+        // constructor to initialise the list
+        public MyList()
+        {
+            numbers = new List<int>();
+        }
+
+        // method to add a number to the list
+        public void AddNumber(int number)
+        {
+            numbers.Add(number);
+        }
+
+        // method to print all the numbers in the list using a for-each loop
+        public void PrintNumbers()
+        {
+            // loop through each number in the list and print it
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
---
+++ L01Opg6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L01Opg6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new MyList();
+            list.AddNumber(1);
+            list.AddNumber(10);
+            list.AddNumber(100);
+            list.PrintNumbers();
+
+            // Opg.7
+            var randomList = new MyList();
+            var rand = new Random();
+
+            Console.WriteLine("\nRandom nums: ");
+            for (int i = 0; i < 10; i++) { 
+                randomList.AddNumber(rand.Next(1, 101));
+            }
+
+            randomList.PrintNumbers();
+            Console.ReadLine();
+        }
+    }
+}
