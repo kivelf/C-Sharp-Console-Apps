0bd6dbf3b1670ab9ab13d6d76de71f841d029a66
Remy <rumena.mp@gmail.com>
2024-08-27 10:51:56 +0200

    opg 4 +5

--- L02Opg4/Program.cs
+++ L02Opg4/Program.cs
@@ -10,6 +10,27 @@ namespace L02Opg4
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What year is the person born in? ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What month is the person born in? ");
+            int month = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What day is the person born on? ");
+            int day = Convert.ToInt32(Console.ReadLine());
+            DateTime input_year = new DateTime(year, month, day);
+            CalculateAge(input_year, out int age);
+            Console.WriteLine($"The person is {age} years old");
+            Console.ReadLine();
+        }
+
+        static void CalculateAge(DateTime BirthDateInput, out int age) {
+            DateTime currentDate = DateTime.Today;
+            age = currentDate.Year - BirthDateInput.Year;
+
+            // if the current date is before the birthday this year, subtract 1
+            if (currentDate < BirthDateInput.AddYears(age))
+            {
+                age--;
+            }
         }
     }
 }
---
+++ L02Opg5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L02Opg5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyNormalMethod(-1);
+            Console.ReadLine();
+        }
+
+        // throws an exception
+        static private void MyMethodWithError(int num) {
+            if (num <= 0) throw new ArgumentException("Antal <= 0");
+            else Console.WriteLine("All is well!");
+        }
+
+        // catches the exception
+        static public void MyNormalMethod(int num) {
+            try
+            {
+                MyMethodWithError(num);
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+            }
+            finally 
+            {
+                Console.WriteLine("We have been in MyNormalMethod()");
+            }
+        }
+    }
+}
