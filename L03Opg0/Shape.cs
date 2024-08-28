b474bd45773d6452aea4ac388602fdd108c3df22
Remy <rumena.mp@gmail.com>
2024-08-28 09:55:32 +0200

    Opg. 3.0 a)

---
+++ L03Opg0/Shape.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L03Opg0
+{
+    public class Shape
+    {
+        private double x, y;
+
+        public Shape(double x, double y) 
+        { 
+            this.x = x;
+            this.y = y;
+        }
+
+        // parameterless constructor that initializes x and y to 1
+        public Shape() : this(1, 1)
+        {
+            // this constructor calls the above constructor with 1.0 for x and y
+        }
+
+        public double X 
+        { 
+            get { return x; }
+            set { x = value; }
+        }
+
+        public double Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+    }
+}
