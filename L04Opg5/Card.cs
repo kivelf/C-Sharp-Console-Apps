05a48ba19ca203173cbda8f0c8eb06bc71d99c38
Remy <rumena.mp@gmail.com>
2024-09-15 14:37:44 +0200

    opg. 4.5

---
+++ L04Opg5/Card.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L04Opg5
+{
+    public enum Suit 
+    { 
+        Clubs = 0,
+        Spades = 1,
+        Diamonds = 2,
+        Hearts = 3
+    }
+
+    public enum CardNumber 
+    { 
+        Ace = 0,
+        Two = 1,
+        Three = 2,
+        Four = 3,
+        Five = 4,
+        Six = 5,
+        Seven = 6,
+        Eight = 7,
+        Nine = 8,
+        Ten = 9,
+        Jack = 10,
+        Queen = 11,
+        King = 12
+    }
+
+    delegate bool FilterCardDelegate(Card card);
+    internal class Card
+    {
+        public Suit Suit {  get; set; }
+        public CardNumber Number { get; set; }
+
+        public Card(Suit suit, CardNumber number)
+        {
+            Suit = suit;
+            Number = number;
+        }
+
+        public override string ToString() 
+        { 
+            return Number + " of " + Suit;
+        }
+    }
+}
---
+++ L04Opg5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L04Opg5
+{
+    internal class Program
+    {
+        static public bool FilterBySpades(Card card) 
+        { 
+            return card.Suit == Suit.Spades;
+        }
+
+        static void Main(string[] args)
+        {
+            CardGame game = new CardGame();
+            game.AddCard(Suit.Hearts, CardNumber.Jack);
+            game.AddCard(Suit.Hearts, CardNumber.Two);
+            game.AddCard(Suit.Hearts, CardNumber.Nine);
+            game.AddCard(Suit.Spades, CardNumber.Five);
+            game.AddCard(Suit.Spades, CardNumber.Ace);
+            game.AddCard(Suit.Spades, CardNumber.Queen);
+            game.AddCard(Suit.Clubs, CardNumber.Six);
+            game.AddCard(Suit.Clubs, CardNumber.Three);
+            game.AddCard(Suit.Clubs, CardNumber.King);
+            game.AddCard(Suit.Diamonds, CardNumber.Four);
+            game.AddCard(Suit.Diamonds, CardNumber.Seven);
+            game.AddCard(Suit.Diamonds, CardNumber.Ten);
+
+            List<Card> cards = game.filterCardGame(FilterBySpades);
+            foreach (Card card in cards) 
+            {
+                Console.WriteLine(card);
+            }
+            Console.WriteLine("------------------------------");
+
+            // using lambda expressions
+            cards = game.filterCardGame(card => card.Number > CardNumber.Ten || card.Number == CardNumber.Ace);
+            foreach (Card card in cards) 
+            { 
+                Console.WriteLine(card);
+            }
+            Console.ReadLine();
+        }
+    }
+}
