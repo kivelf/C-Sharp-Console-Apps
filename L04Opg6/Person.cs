a11abfe68d9bf631f8fda3938608a10b4b93b295
Remy <rumena.mp@gmail.com>
2024-09-15 13:26:03 +0200

    opg. 4.6

---
+++ L04Opg6/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L04Opg6
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Weight { get; set; }
+
+        public Person(string name, int age, double weight) 
+        {
+            Name = name;
+            Age = age;
+            Weight = weight;
+        }
+
+        public override string ToString() 
+        { 
+            return $"Person: {Name}, {Age} years old, weighs {Weight}kgs";
+        }
+    }
+}
---
+++ L04Opg6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L04Opg6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            people.Add(new Person("Caroline", 44, 62.5));
+            people.Add(new Person("Anne", 21, 57.8));
+            people.Add(new Person("Emil", 48, 82.7));
+            people.Add(new Person("Diana", 28, 66.3));
+            people.Add(new Person("Bob", 36, 75.2));
+            
+            foreach (Person person in people) 
+            {
+                Console.WriteLine(person);
+            }
+            Console.WriteLine("------------------------------");
+
+            // sort by age
+            people.Sort(new ByAgeSorter());
+            foreach (Person person in people)
+            {
+                Console.WriteLine(person);
+            }
+            Console.WriteLine("------------------------------");
+
+            // using delegates/lambda expressions
+            people.Sort((Person p1, Person p2)=> p1.Weight.CompareTo(p2.Weight));
+            foreach (Person person in people)
+            {
+                Console.WriteLine(person);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
