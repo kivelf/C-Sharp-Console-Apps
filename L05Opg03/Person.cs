6e3e7aa6fbfe4c130b57b921cf85185d59cf8c17
Remy <rumena.mp@gmail.com>
2024-09-17 09:31:24 +0200

    opg. 5.3

---
+++ L05Opg03/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L05Opg03
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Weight { get; set; }
+        public int Score { get; set; }
+        public bool Accepted { get; set; }
+
+        // constructor which uses the data from the file
+        public Person(string data)
+        {
+            // Name, Age, Weight, Score
+            var L = data.Split(';');
+            Name = L[0];
+            Age = int.Parse(L[1]);
+            Weight = int.Parse(L[2]);
+            Score = int.Parse(L[3]);
+            Accepted = false; //denne parameter bliver ikke indlæst, men skal bruges senere
+        }
+
+        public static List<Person> ReadCSVFile(string filename)
+        {
+            List<Person> list = new List<Person>();
+            using (var file = new StreamReader(filename))
+            {
+                string line;
+                while ((line = file.ReadLine()) != null)
+                {
+                    var p = new Person(line);
+                    list.Add(p);
+                    //Console.WriteLine(p);
+                }
+            }
+            return list;
+        }
+
+        public override string ToString()
+        {
+            return $"Person: {Name}, {Age} years old, weighs {Weight}kgs, score: {Score}, accepted: {Accepted}";
+        }
+    }
+}
---
+++ L05Opg03/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L05Opg03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people1 = new List<Person>();
+
+            try
+            {
+                people1 = Person.ReadCSVFile(@"D:\Datamatiker\4 semester\C-Sharp and Dot Net\Projects\Console Apps\L05Opg03\data1-1.csv");
+                Console.WriteLine("Data successfully loaded");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("EXCEPTION: " + ex.Message);
+                Console.WriteLine("You should probably set the filename to the Person.ReadCSVFile method to something on your disk!");
+            }
+
+            Console.WriteLine("Alle personer med en score under 2:");
+            var people2 = people1.FindAll(p => p.Score < 2);
+            foreach (Person p in people2) 
+            {
+                Console.WriteLine(p);
+            }
+            Console.WriteLine("------------------------------");
+
+            Console.WriteLine("Alle personer med en lige score:");
+            people2 = people1.FindAll(p => p.Score % 2 == 0);
+            foreach (Person p in people2)
+            {
+                Console.WriteLine(p);
+            }
+            Console.WriteLine("------------------------------");
+
+            Console.WriteLine("Alle personer med en lige score og weight større end 60:");
+            people2 = people1.FindAll(p => p.Score % 2 == 0 && p.Weight > 60);
+            foreach (Person p in people2)
+            {
+                Console.WriteLine(p);
+            }
+            Console.WriteLine("------------------------------");
+
+            Console.WriteLine("Alle personer med en vægt deleligt med 3:");
+            people2 = people1.FindAll(p => p.Weight % 3 == 0);
+            foreach (Person p in people2)
+            {
+                Console.WriteLine(p);
+            }
+            Console.ReadLine();
+        }
+    }
+}
